3861134e242f758ae20fe44c995db44b43c8960a
Rohan <rohanpujaris@gmail.com>
2014-06-20 22:49:27 +0530

    20/06 3

--- problem1.rb
+++ problem1.rb
@@ -1,13 +1,16 @@
 def problem(array, k, step)
   if array.length == 1
-  	return array
+  	return 
   end
+  puts "step:#{step}"
   array.select! { |n| n % step == 0 }
   p array
-  problem(array, k, step*k)	
+  problem(array, k, step*2)	
 end
 array = []
-contestants = 12
-remove_interval = 2
+puts "Enter number of contestants"
+contestants = gets.chomp.to_i
+puts "Enter interval of selection"
+remove_interval = gets.chomp.to_i
 (1..contestants).each { |n| array << n }
-p problem(array, remove_interval, remove_interval )
+problem(array, remove_interval, remove_interval )
